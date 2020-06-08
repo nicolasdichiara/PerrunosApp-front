a8105d208fb080c216967302811f19ef4f870e00
Maximiliano Bianco <mbianco@grupopiero.com>
2020-06-07 23:18:50 -0300

    Agregar mascota form y validaciones

--- src/app/mascota-add/mascota-add.page.ts
+++ src/app/mascota-add/mascota-add.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Mascota } from 'src/domain/mascota';
+
 
 @Component({
   selector: 'app-mascota-add',
@@ -7,9 +10,118 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MascotaAddPage implements OnInit {
 
-  constructor() { }
+  today: any;
+  mascota: Mascota;
+  edad = 0;
+
+  get nombre() {
+    return this.registrationForm.get('nombre');
+  }
+
+  get descripcion() {
+    return this.registrationForm.get('descripcion');
+  }
+
+  get cuidadosEsp() {
+    return this.registrationForm.get('cuidadosEsp');
+  }
+
+  get enfermedadesPrev() {
+    return this.registrationForm.get('enfermedadesPrev');
+  }
+
+  get fechaNacimiento() {
+    return this.registrationForm.get('fechaNacimiento');
+  }
+
+  get desparasitado() {
+    return this.registrationForm.get('desparasitado');
+  }
+
+  get paseoAlgunaVez() {
+    return this.registrationForm.get('paseoAlgunaVez');
+  }
+
+  get paseoConPaseadores() {
+    return this.registrationForm.get('paseoConPaseadores');
+  }
+
+  get interactua() {
+    return this.registrationForm.get('interactua');
+  }
+
+  public errorMessages = {
+    nombre: [
+      { type: 'required', message: 'Nombre es requerido'},
+      { type: 'maxlength', message: 'Nombre no puede ser mayor que 50 caracteres'},
+    ],
+    descripcion: [
+      { type: 'required', message: 'descripcion es requerido'},
+      { type: 'maxlength', message: 'descripcion no puede ser mayor que 100 caracteres'},
+    ],
+    cuidadosEsp: [
+      { type: 'maxlength', message: 'Cuidados Esp no puede ser mayor que 100 caracteres'},
+    ],
+    enfermedadesPrev: [
+      { type: 'maxlength', message: 'Enfermedades Previas no puede ser mayor que 100 caracteres'},
+    ],
+    fechaNacimiento: [
+      { type: 'required', message: 'Fecha de nacimiento es requerido'},
+    ],
+    desparasitado: [
+      { type: 'required', message: 'desparasitado es requerido'},
+    ],
+    paseoAlgunaVez: [
+      { type: 'required', message: 'paseoAlgunaVez es requerido'},
+    ],
+    paseoConPaseadores: [
+      { type: 'required', message: 'paseoConPaseadores es requerido'},
+    ],
+    interactua: [
+      { type: 'required', message: 'interactua es requerido'},
+    ]
+  };
+
+  registrationForm = this.formBuilder.group({
+    nombre: ['', [Validators.required, Validators.maxLength(50)]],
+    descripcion: ['', [Validators.required, Validators.maxLength(100)]],
+    cuidadosEsp: ['', [Validators.maxLength(100)]],
+    enfermedadesPrev: ['', [Validators.maxLength(100)]],
+    fechaNacimiento: ['', [Validators.required]],
+    desparasitado: ['', [Validators.required]],
+    paseoAlgunaVez: ['', [Validators.required]],
+    paseoConPaseadores: ['', [Validators.required]],
+    interactua: ['', [Validators.required]],
+  });
+
+  constructor(private formBuilder: FormBuilder) {
+    this.today = new Date().toISOString();
+  }
+
+  public submit(){
+    console.log(this.registrationForm.value);
+    this.mascota = new Mascota();
+    this.mascota.nombre = this.registrationForm.get('nombre').value;
+    this.mascota.descripcion = this.registrationForm.get('descripcion').value;
+    this.mascota.cuidadosEsp = this.registrationForm.get('cuidadosEsp').value;
+    this.mascota.enfermedadesPrev = this.registrationForm.get('enfermedadesPrev').value;
+    this.mascota.fechaNacimiento = this.registrationForm.get('fechaNacimiento').value;
+    this.mascota.desparasitado = this.registrationForm.get('desparasitado').value;
+    this.mascota.paseoAlgunaVez = this.registrationForm.get('paseoAlgunaVez').value;
+    this.mascota.interactua = this.registrationForm.get('interactua').value;
+    this.mascota.paseoConPaseadores = this.registrationForm.get('paseoConPaseadores').value;
+  }
 
   ngOnInit() {
+  }
+
+  calcEdad(){
+    let today: any = new Date();
+    let selectedDate: any = new Date(this.registrationForm.get('fechaNacimiento').value);
+
+    let edad = Math.round((Math.abs(selectedDate - today) / (24 * 60 * 60 * 1000)) / 365);
+
+    this.edad = edad;
   }
 
 }
--- src/domain/mascota.ts
+++ src/domain/mascota.ts
@@ -1,8 +1,32 @@
 export class Mascota {
+
     id: number;
     nombre: string;
     cuidadosEsp: string;
     descripcion: string;
-    enfermedadesPrevias: string;
+    enfermedadesPrev: string;
     fechaNacimiento: Date;
+    desparasitado: number;
+    paseoAlgunaVez: number;
+    paseoConPaseadores: number;
+    interactua: number;
+
+    static fromJson(individuoJSON): Mascota {
+        return Object.assign(new Mascota(), individuoJSON);
+    }
+
+    constructor(_nombre?: string, _cuidadosEsp?: string, _descripcion?: string, _enfermedadesPrev?: string,
+         _fechaNacimiento?: Date, _desparasitado?: number, _paseoAlgunaVez?: number, _paseoConPaseadores?: number, _interactua?: number) {
+      this.nombre = _nombre;
+      this.cuidadosEsp = _cuidadosEsp;
+      this.descripcion = _descripcion;
+      this.enfermedadesPrev = _enfermedadesPrev;
+      this.fechaNacimiento = _fechaNacimiento;
+      this.desparasitado = _desparasitado;
+      this.paseoAlgunaVez = _paseoAlgunaVez;
+      this.paseoConPaseadores = _paseoConPaseadores;
+      this.interactua = _interactua;
+
+    }
+
 }
