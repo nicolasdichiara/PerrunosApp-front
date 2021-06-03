19554b06ab528f64102f83a88047fea228b6f953
Nicolas <nicolasdichiara@hotmail.com>
2021-06-03 20:14:57 -0300

    Implemento el deep linking

---
+++ src/app/app.ts
@@ -0,0 +1,28 @@
+ import { NavController, Platform } from "@ionic/angular";
+ import { Deeplinks } from "@ionic-native/deeplinks";
+ import { HomePage } from "./home/home.page";
+import { MascotaDetailPageModule } from "./mascota-detail/mascota-detail.module";
+
+
+export class PerrunosApp {
+  constructor(
+    protected platform: Platform
+    , protected navController: NavController
+    , protected deeplinks: Deeplinks
+    ) {
+    this.platform.ready().then(() => {
+      this.deeplinks.route({
+        '/home/mascota-detail/:id': MascotaDetailPageModule
+      }).subscribe((match) => {
+        // match.$route - the route we matched, which is the matched entry from the arguments to route()
+        // match.$args - the args passed in the link
+        // match.$link - the full link data
+        console.log('Successfully matched route', match);
+      },
+      (nomatch) => {
+        // nomatch.$link - the full link data
+        console.error('Got a deeplink that didn\'t match', nomatch);
+      });
+    });
+  }
+}
